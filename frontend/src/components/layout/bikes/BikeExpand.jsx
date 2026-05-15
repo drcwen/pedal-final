6f8eb2c250d01c2ef2105798945f495010bc0e6e
Wendel Derraco <wendelderraco@gmail.com>
2026-05-16 00:16:28 +0800

    Added time in rent nav

--- frontend/src/components/layout/bikes/BikeExpand.jsx
+++ frontend/src/components/layout/bikes/BikeExpand.jsx
@@ -4,6 +4,8 @@ import { supabase } from "../../../lib/supabase"
 import { useLocation } from "react-router-dom";
 import Navigation from "../Navigation/StaticNavigationPC"
 import AdjustNumber from "../../ui/AdjustNumber"
+import { IoMdArrowRoundBack } from "react-icons/io";
+
 
 function BikeExpand() {
 
@@ -18,44 +20,76 @@ function BikeExpand() {
         <>
 
         <Navigation />
-        
-        <div className='box-model flex flex-col gap-10 items-center justify-center'>
-            <div className='grid grid-cols-2 gap-20 items-center'>
-                <div className=''>
-                    <img src={bike.image_url} className='w-80'/>
-                </div>
 
-                <div className='flex flex-col gap-5'>
+        {/*Back Button*/}
 
-                    <div className='flex flex-col gap-2'>
-                        <h1 className='text-4xl font-akagi font-black text-darkblue'>{bike.name}</h1>
-                        
-                        {/*Bikes Available*/}
-                        <div className='bg-yellow rounded-lg w-fit px-3 py-1'>
-                            <h1 className='text-md font-bold font-akagi text-navyblue'>{bike.available_count} units available</h1>
-                        </div>
+            <div className='box-model flex flex-col gap-10 items-center justify-center'>
+
+                {/*Grid*/}
+                <div className='lg:grid lg:grid-cols-2 lg:gap-30 gap-10 flex flex-col items-center'>
+                    <div className=''>
+                        <img src={bike.image_url} className='w-80'/>
                     </div>
 
-                    <div className='grid grid-cols-2 gap-2'>
+                    <div className='flex flex-col gap-5'>
 
-                        {/*Quanttiy*/}
-                        <div className='flex flex-row gap-5'>
-                            <h1 className='font-akagi font-semibold text-2xl text-[#979B9D]'>Quantity</h1>
+                        <div className='flex flex-col gap-2 items-center lg:items-start'>
+                            <h1 className='text-4xl font-akagi font-black text-darkblue'>{bike.name}</h1>
                             
-                            <AdjustNumber value={quantity} setValue={setQuantity} color={"[#979B9D]"}/>
+                            {/*Bikes Available*/}
+                            <div className='bg-yellow rounded-lg w-fit px-3 py-1'>
+                                <h1 className='text-md font-bold font-akagi text-navyblue'>{bike.available_count} units available</h1>
+                            </div>
                         </div>
 
-                        {/*Quanttiy*/}
-                        <div className='flex flex-row gap-5'>
-                            <h1 className='font-akagi font-semibold text-2xl text-[#979B9D]'>Hours</h1>
-                            
-                            
-                        </div>
+                        <div className="flex">
+                            <div className="grid grid-cols-2 lg:gap-x-6 gap-y-4 items-center w-full">
+
+                                {/* Quantity */}
+                                <h1 className="font-akagi font-semibold text-lg lg:text-2xl text-[#979B9D]">
+                                    Quantity
+                                </h1>
+                                <div>
+                                    <AdjustNumber value={quantity} setValue={setQuantity} />
+                                </div>
+
+                                {/* Hours */}
+                                <h1 className="font-akagi font-semibold text-lg lg:text-2xl text-[#979B9D]">
+                                    Hours
+                                </h1>
+
+                                <div className="border-2 border-[#979B9D] rounded-lg flex items-center justify-center lg:py-1 px-4">
+                                    <h1 className="text-lg font-bold font-akagi text-[#979B9D]">
+                                        {reservationData.hours}
+                                    </h1>
+                                </div>
+
+                                {/* Date */}
+                                <h1 className="font-akagi font-semibold text-lg lg:text-2xl text-[#979B9D]">
+                                    Date
+                                </h1>
+                                <div className="border-2 border-[#979B9D] rounded-lg flex items-center justify-center lg:py-1 px-4">
+                                    <h1 className="text-lg font-bold font-akagi text-[#979B9D]">
+                                        {reservationData.date}
+                                    </h1>
+                                </div>
+
+                                {/* Date */}
+                                <h1 className="font-akagi font-semibold text-lg lg:text-2xl text-[#979B9D]">
+                                    Time
+                                </h1>
+                                <div className="border-2 border-[#979B9D] rounded-lg flex items-center justify-center lg:py-1 px-4">
+                                    <h1 className="text-lg font-bold font-akagi text-[#979B9D]">
+                                        {reservationData.startTime}
+                                    </h1>
+                                </div>
+
+                            </div>
+                            </div>
                     </div>
-                </div>
 
+                </div>
             </div>
-        </div>
         {/*<div>
             <h1>{bike.name}</h1>
             <img src={bike.image_url} alt={bike.name} />
--- frontend/src/components/ui/AdjustNumber.jsx
+++ frontend/src/components/ui/AdjustNumber.jsx
@@ -1,7 +1,7 @@
 import { FiMinus } from "react-icons/fi";
 import { FiPlus } from "react-icons/fi";
 
-function AdjustNumber({ value, setValue, color }) {
+function AdjustNumber({ value, setValue }) {
 
     const addValue = () => {
         setValue((prev) => prev + 1);
@@ -14,17 +14,17 @@ function AdjustNumber({ value, setValue, color }) {
     };
 
     return(
-        <div className={`grid grid-cols-3 rounded-lg border-2 border-${color}`}>
+        <div className={`grid grid-cols-3 rounded-lg border-2 border-[#979B9D]`}>
 
             <div
                 className='flex items-center justify-center px-2 cursor-pointer'
                 onClick={subtractValue}
             >
-                <FiMinus className={`text-${color}`}/>
+                <FiMinus className={`text-[#979B9D]`}/>
             </div>
 
-            <div className={`bg-${color} flex justify-center items-center`}>
-                <h1 className='text-lg text-white font-akagi font-semibold'>
+            <div className={`bg-[#979B9D] flex justify-center items-center lg:py-1`}>
+                <h1 className='text-lg text-[#ffffff] font-akagi font-semibold'>
                     {value}
                 </h1>
             </div>
@@ -33,7 +33,7 @@ function AdjustNumber({ value, setValue, color }) {
                 className='flex items-center justify-center cursor-pointer'
                 onClick={addValue}
             >
-                <FiPlus className={`text-${color}`}/>
+                <FiPlus className={`text-[#979B9D]`}/>
             </div>
 
         </div>
