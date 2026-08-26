a9670f4c64fbd33bf5852aacfd8ebd5c854a763e
Wendel Derraco <wendelderraco@gmail.com>
2026-08-27 02:19:48 +0800

    Added progress to Settings

--- frontend/src/components/management/settings/Settings.jsx
+++ frontend/src/components/management/settings/Settings.jsx
@@ -11,6 +11,14 @@ function Settings() {
 
     const [activeTab, setActiveTab] = useState("operatingHours");
 
+    const [percent, setPercent] = useState("%");
+
+    const [paymentOccurence, setPaymentOccurence] = useState("Periodic");
+
+    const handleChange = (event) => {
+        setPaymentOccurence(event.target.value);
+    };
+
   return (
     <>
 
@@ -63,31 +71,11 @@ function Settings() {
                                     <h1 className='md:text-md text-sm font-akagi font-medium text-gray'>The unchecked date below will be unavailable for reservation.</h1>
                                 </div>
 
-                                <div className="w-full md:flex md:flex-col md:justify-center md:items-center">
-                                    <div className="w-full md:grid md:grid-cols-[150px_1fr] flex flex-col font-akagi font-medium text-gray gap-2 md:items-center">
+                                <div className="w-full flex flex-col font-akagi font-medium text-gray gap-2">
 
-                                        <h1>Monday</h1>
+                                    <h1>Monday</h1>
 
-                                        <div className="flex justify-between">
-                                            <input
-                                                type="time"
-                                                min="08:00"
-                                                max="22:00"
-                                                step="1800"
-                                                className="border border-gray rounded-lg px-3 py-1 focus:outline-none"
-                                            />
-
-                                            <input
-                                                type="time"
-                                                min="08:00"
-                                                max="22:00"
-                                                step="1800"
-                                                className="border border-gray rounded-lg px-3 py-1 focus:outline-none"
-                                            />
-                                            
-                                        </div>
-
-                                    </div>
+                                   
                                 </div>
                             </div>
 
@@ -95,16 +83,89 @@ function Settings() {
                     }
 
                     {activeTab === "revenueDeductions" &&
-                        <motion.div
-                            initial={{ height: 0, opacity: 0 }}
-                            animate={{ height: "auto", opacity: 1 }}
-                            exit={{ height: 0, opacity: 0 }}
-                            transition={{ duration: 0.3, ease: "easeInOut" }}
-                            className="w-full flex-1 min-h-0 grid grid-cols-2 rounded-xl bg-[#ebebeb] border border-gray/60"
-                        >
-                            
-                            
-                        </motion.div>
+                        <>
+                            <motion.div
+                                initial={{ height: 0, opacity: 0 }}
+                                animate={{ height: "auto", opacity: 1 }}
+                                exit={{ height: 0, opacity: 0 }}
+                                transition={{ duration: 0.3, ease: "easeInOut" }}
+                                className="w-full rounded-xl bg-[#ebebeb] border border-gray/60"
+                            >
+                                <div className='w-full p-5 md:border-r-1 md:border-gray/60 flex flex-col gap-5'>
+
+                                    {/*Title*/}
+                                    <div className='w-full flex flex-col gap-1'>
+                                        <h1 className='md:text-2xl text-xl font-akagi font-bold tracking-wide text-blue'>Add a Revenue Deduction</h1>
+                                    </div>
+
+                                    <div className='w-full lg:grid lg:grid-cols-2 flex flex-col gap-3 font-akagi font-bold text-gray'>
+                                        <div className='flex flex-col gap-1'>
+                                            <h1>Type</h1>
+                                            <input type='text' className='px-2 py-1 rounded-lg border border-gray focus:outline-none'/>
+                                        </div>
+
+                                        <div className='flex flex-col gap-1'>
+                                            <h1>Percentage/Amount</h1>
+                                            <div className='flex flex-row gap-3'>
+                                                <input type='text' className='w-full px-2 py-1 rounded-lg border border-gray focus:outline-none'/>
+                                                <div className='grid grid-cols-2 rounded-lg border border-blue'>
+                                                    <div 
+                                                        onClick={() => {setPercent("%")}}
+                                                        className={`${percent === "%" ? "bg-blue text-[#ffffff]" : "text-blue"} px-4 py-1 rounded-tl-md rounded-bl-md`}>
+                                                        %
+                                                    </div>
+
+                                                    <div 
+                                                        onClick={() => {setPercent("P")}}
+                                                        className={`${percent === "P" ? "bg-blue text-[#ffffff]" : "text-blue"} px-4 py-1 rounded-tr-md rounded-br-md text-[#ffffff]`}>
+                                                        P
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className='flex flex-col gap-1'>
+                                            <h1>Payment Occurence</h1>
+                                            <div className='flex flex-row gap-10 px-3'>
+                                                <div className='flex flex-row gap-1'>
+                                                    <input type='radio' value='Periodic' checked={paymentOccurence === 'Periodic'} onChange={handleChange}/>
+                                                    <h1 className='font-medium'>Periodic</h1>
+                                                </div>
+
+                                                <div className='flex flex-row gap-1'>
+                                                    <input type='radio' value='One-time' checked={paymentOccurence === 'One-time'} onChange={handleChange}/>
+                                                    <h1 className='font-medium'>One-time</h1>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        {paymentOccurence === "Periodic" &&
+                                            <div className='flex flex-row gap-5'>
+                                                
+                                            </div>
+                                        }
+                                    </div>
+
+                                </div>
+                            </motion.div>
+
+                            <motion.div
+                                initial={{ height: 0, opacity: 0 }}
+                                animate={{ height: "auto", opacity: 1 }}
+                                exit={{ height: 0, opacity: 0 }}
+                                transition={{ duration: 0.3, ease: "easeInOut" }}
+                                className="w-full rounded-xl bg-[#ebebeb] border border-gray/60"
+                            >
+                                <div className='w-full p-5 md:border-r-1 md:border-gray/60 flex flex-col gap-5'>
+
+                                    {/*Title*/}
+                                    <div className='w-full flex flex-col gap-1'>
+                                        <h1 className='md:text-2xl text-xl font-akagi font-bold tracking-wide text-blue'>Add a Revenue Deduction</h1>
+                                    </div>
+
+                                </div>
+                            </motion.div>
+                        </>
                     }
                     
                 </div>
